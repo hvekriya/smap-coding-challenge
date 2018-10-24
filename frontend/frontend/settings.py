21339a626d107950c37c6554c3d90b8a66b69917
hvekriya <h.vekriya@kainos.com>
2018-10-24 09:27:25 +0100

    Updated to use Vue webpack with sass loaders

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'app.apps.AppConfig',
     'rest_framework',
+    'webpack_loader',
 ]
 
 MIDDLEWARE = [
@@ -120,4 +121,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR + '/app/', 'dist'),
+    os.path.join(BASE_DIR + '/app/', 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR + '/app/', 'public')
 STATIC_URL = '/static/'
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': '',
+        'STATS_FILE': os.path.join(BASE_DIR + '/app/', 'webpack-stats.json'),
+    }
+}
